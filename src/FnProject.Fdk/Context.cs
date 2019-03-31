5ce6395d75a00befab29745d9809d9c8fd8b1669
Daniel Lo Nigro <d@d.sb>
2019-03-31 11:38:46 -0700

    Add HTTP headers to Context

--- src/FnProject.Fdk/Context.cs
+++ src/FnProject.Fdk/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using Microsoft.AspNetCore.Http;
 
 namespace FnProject.Fdk
 {
@@ -7,14 +8,53 @@ namespace FnProject.Fdk
 	/// </summary>
 	public class Context : IContext
 	{
-		private readonly IHttpContextAccessor _httpContextAccessor;
-		
-		public IConfig Config { get; }
+		private readonly HttpRequest _request;
 
 		public Context(IConfig config, IHttpContextAccessor httpContextAccessor)
 		{
-			_httpContextAccessor = httpContextAccessor;
+			_request = httpContextAccessor.HttpContext.Request;
 			Config = config;
 		}
+
+		/// <summary>
+		/// Gets the unique ID assigned to this request.
+		/// </summary>
+		public string CallId => _request.Headers["Fn-Call-Id"];
+
+		/// <summary>
+		/// Gets the system configuration.
+		/// </summary>
+		public IConfig Config { get; }
+
+		/// <summary>
+		/// Get the date/time after which the request will be aborted.
+		/// </summary>
+		public DateTime? Deadline
+		{
+			get
+			{
+				var rawHeader = _request.Headers["Fn-Deadline"];
+				if (rawHeader.Count == 0)
+				{
+					return null;
+				}
+				return DateTime.Parse(rawHeader);
+			}
+		}
+
+		/// <summary>
+		/// Get all HTTP headers for the request
+		/// </summary>
+		public IHeaderDictionary Headers => _request.Headers;
+
+		/// <summary>
+		/// Gets the method for this event
+		/// </summary>
+		public string Method => _request.Headers["Fn-Http-Method"];
+
+		/// <summary>
+		/// Gets the HTTP request URL for this event
+		/// </summary>
+		public string RequestUrl => _request.Headers["Fn-Http-Request-Url"];
 	}
 }
--- src/FnProject.Fdk/IContext.cs
+++ src/FnProject.Fdk/IContext.cs
@@ -1,6 +1,41 @@
-﻿namespace FnProject.Fdk
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace FnProject.Fdk
 {
+	/// <summary>
+	/// Represents the context that a function is executing in
+	/// </summary>
 	public interface IContext
 	{
+		/// <summary>
+		/// Gets the unique ID assigned to this request.
+		/// </summary>
+		string CallId { get; }
+
+		/// <summary>
+		/// Gets the system configuration.
+		/// </summary>
+		IConfig Config { get; }
+
+		/// <summary>
+		/// Get the date/time after which the request will be aborted.
+		/// </summary>
+		DateTime? Deadline { get; }
+
+		/// <summary>
+		/// Get all HTTP headers for the request
+		/// </summary>
+		IHeaderDictionary Headers { get; }
+
+		/// <summary>
+		/// Gets the method for this event
+		/// </summary>
+		string Method { get; }
+
+		/// <summary>
+		/// Gets the HTTP request URL for this event
+		/// </summary>
+		string RequestUrl { get; }
 	}
 }
